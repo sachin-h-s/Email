5271d7e2c2fc0fc4959197fb2372c4b7babb5237
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-11 10:34:20 +0530

    Create email_generator.py

---
+++ email_generator.py
@@ -0,0 +1,36 @@
+import openai_secret_manager
+import streamlit as st
+import openai
+
+# Use the openai API key
+openai.api_key = openai_secret_manager.get_secret("openai")["api_key"]
+
+# Create a function that uses GPT-3 to generate text
+def generate_text(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text
+    return message.strip()
+
+def write_email():
+    reason = st.selectbox("Select the reason for the email", ["Job Application", "Business Proposal", "Complaint", "Inquiry"])
+    recipient = st.text_input("Recipient's Email Address:")
+    st.write("Please provide more details about the email")
+    prompt = (f"Write an email for the reason {reason}")
+    details = st.text_area("Enter your message here")
+    prompt += ' '+ details
+    # Use GPT-3 to generate the email
+    message = generate_text(prompt)
+    st.write("Generated Email:")
+    st.write(message)
+
+if __name__=="__main__":
+    st.title("Email Writer using GPT-3")
+    write_email()
