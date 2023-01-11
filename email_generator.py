390d91073aedb3c710f0f996348e1cb236acae12
sachin-h-s <89412649+sachin-h-s@users.noreply.github.com>
2023-01-11 11:22:27 +0530

    Update email_generator.py

--- email_generator.py
+++ email_generator.py
@@ -23,17 +23,22 @@ def write_email():
     api_key = st.text_input("Please enter OpenAI API key", "")
     openai.api_key = api_key
 
-    reason = st.selectbox("Select the reason for the email", ["Job Application", "Business Proposal", "Complaint", "Inquiry"])
-    recipient = st.text_input("Recipient's Email Address:")
-    st.write("Please provide more details about the email")
-    prompt = (f"Write an email for the reason {reason}")
-    details = st.text_area("Enter your message here")
-    prompt += ' '+ details
-    # Use GPT-3 to generate the email
-    message = generate_text(prompt)
-    st.write("Generated Email:")
-    st.write(message)
-
+#     reason = st.selectbox("Select the reason for the email", ["Job Application", "Business Proposal", "Complaint", "Inquiry"])
+#     recipient = st.text_input("Recipient's Email Address:")
+#     st.write("Please provide more details about the email")
+#     prompt = (f"Write an email for the reason {reason}")
+#     details = st.text_area("Enter your message here")
+#     prompt += ' '+ details
+#     # Use GPT-3 to generate the email
+#     message = generate_text(prompt)
+#     st.write("Generated Email:")
+#     st.write(message)
+    subject = st.text_input("Please enter email subject:", "")
+    prompt = (f"Write a email with the subject: {subject}")
+    message = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=2048, n =1,stop=None,temperature=0.5)
+    st.write("Email Message:")
+    st.write(message.choices[0].text)
+    
 if __name__=="__main__":
     st.title("Email Writer using GPT-3")
     write_email()
